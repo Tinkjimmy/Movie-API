e5e28138f482d015bd1333f2882ce889a0bb22e2
Tinkjimmy <tinkjimmy938@gmail.com>
2023-03-29 21:36:09 +0200

    adding array of movies and updating express.static

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const express = require('express'),
 
 const app = express();
 
+const Top10movies = [
+  { title: "The Shawshank Redemption", director: "Frank Darabont" },
+  { title: "The Godfather", director: "Francis Ford Coppola" },
+  { title: "The Godfather: Part II", director: "Francis Ford Coppola" },
+  { title: "The Dark Knight", director: "Christopher Nolan" },
+  { title: "12 Angry Men", director: "Sidney Lumet" },
+  { title: "Schindler's List", director: "Steven Spielberg" },
+  { title: "The Lord of the Rings: The Return of the King", director: "Peter Jackson" },
+  { title: "Pulp Fiction", director: "Quentin Tarantino" },
+  { title: "The Good, the Bad and the Ugly", director: "Sergio Leone" },
+  { title: "Forrest Gump", director: "Robert Zemeckis" }
+];
 
 
 app.use(morgan('common'));
-app.use('/public', express.static('documentation.html'));
+app.use(express.static("public"));
 
 // GET requests
 app.get('/movies', (req, res) => {
