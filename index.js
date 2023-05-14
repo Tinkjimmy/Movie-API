f150b3a01dd251984cc88caa28256dc7e847354d
Tinkjimmy <tinkjimmy938@gmail.com>
2023-05-14 23:40:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,22 @@ app.use(cors());
 
 const { check, validationResult } = require("express-validator");
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
+
 //Models
 app.use(bodyParser.json());
 // mongoose.connect('mongodb://127.0.0.1:27017/moviesDB', { useNewUrlParser: true, useUnifiedTopology: true });
