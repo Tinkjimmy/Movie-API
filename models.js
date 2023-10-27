304b2bbbf92c7450d4ad80dfb104cee227b7ff76
Tinkjimmy <tinkjimmy938@gmail.com>
2023-10-27 20:59:03 +0200

    ok

--- models.js
+++ models.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-const { app, Users } = require(".");
-const { check, validationResult } = require("express-validator");
 
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
@@ -43,53 +41,3 @@ let User = mongoose.model("User", userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
-// CREATE new user
-app.post(
-  "/users",
-  [
-    check("Username", "Username must be at least 5 characters long").isLength({
-      min: 5,
-    }),
-    check(
-      "Username",
-      "Username contains non alphanumeric characters - not allowed."
-    ).isAlphanumeric(),
-    check("Password", "Password is required").not().isEmpty(),
-    check("Email", "Email does not appear to be valid").isEmail(),
-    check("Birth", "birthdate must be a date").isDate(),
-  ],
-  // passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username: req.body.Username })
-      .then((user) => {
-        if (user) {
-          return res.status(400).send(req.body.Username + "already exists");
-        } else {
-          Users.create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-
-            Email: req.body.Email,
-            Birth: req.body.Birth,
-          })
-            .then((user) => {
-              res.status(201).json(user);
-            })
-            .catch((error) => {
-              console.error(error);
-              res.status(500).send("Error: " + error);
-            });
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error: " + error);
-      });
-  }
-);
