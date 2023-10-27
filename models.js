114b6e2a86b0552649ef0e7a50196d43d3c5d674
Tinkjimmy <tinkjimmy938@gmail.com>
2023-10-27 19:48:49 +0200

    Update models.js

--- models.js
+++ models.js
@@ -23,7 +23,7 @@ let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
-  // Birth: Date,
+  Birth: Date,
 
   Favourites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
